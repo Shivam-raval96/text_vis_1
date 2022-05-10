09a6e3a3cf0e08972276ddf2a4c732f2beb0d565
Shivam-raval96 <sraval@g.harvard.edu>
2022-05-10 10:36:09 -0400

    added obama viz

---
+++ names.js
@@ -0,0 +1,189 @@
+paths = [
+  'csv/algore_ted3.csv',
+  'csv/Weekly Address Time for Congress to Raise the Minimum Wage for the American People3.csv',
+'csv/Weekly Address The President\'s Budget Ensures Opportunity for All Hard-Working Americans3.csv',
+'csv/Weekly Address The President\'s Year of Action3.csv',
+'csv/Weekly Address Wishing the American People a Happy Thanksgiving3.csv',
+  'csv/ps_Texting and Driving Persuasive Speech3.csv',
+  'csv/ps_Why students should have mental health days  Hailey Hardcastle3.csv',
+  'csv/ps_Why school should start later for teens  Wendy Troxel3.csv',
+  'csv/ds_My demonstration speech on how to solve the Rubik\'s cube3.csv',
+  'csv/ds_Demonstrative Speech How to Make Frozen Hot Choocolate3.csv',
+  'csv/ds_CPR Demonstration Speech3.csv',
+  'csv/is_After watching this, your brain will not be the same  Lara Boyd  TEDxVancouver3.csv',
+  'csv/is_Informative Speech Star Wars3.csv',
+  'csv/is_"About ALS" - An Informative Speech3.csv',
+  'csv/es_Ali Wong Stand-Up Performance - Late Night with Seth Meyers3.csv',
+  'csv/es_George Carlin - Everyday expressions (that don\'t make sense)3.csv',
+  'csv/es_Jerry Seinfeld Does His Best Tight Five3.csv',
+  'csv/trump_2004_speech3.csv',
+  'csv/trump_2020_speech3.csv',
+  'csv/trump_2020_interview3.csv',
+  'csv/trump_2010_interview3.csv',
+  'csv/obama_2010_interview3.csv',
+  'csv/obama_2020_interview3.csv',
+  'csv/obama_2004_speech3.csv',
+  'csv/obama_2021_speech3.csv',
+  "csv/kennedy_nixon_debate_part3.csv",
+  "csv/kennedy_rice3.csv",
+  "csv/nixon_vietnam_19693.csv",
+  "csv/news_fox3.csv",
+  "csv/news_lester_holt3.csv",
+  "csv/news_cnn_debate3.csv",
+  "csv/news_nbc_20003.csv",
+  //"csv/news_nbc_19902.csv",
+  "csv/news_nbc_19803.csv",
+  "csv/news_nbc_19703.csv",
+  "csv/news_nbc_19603.csv",
+  "csv/finale_talk_20173.csv",
+  "csv/finale_talk_20213.csv",
+  "csv/martin_hbs3.csv",
+  "csv/fernanda_hbs3.csv",
+  "csv/viz_talk_20103.csv",
+  "csv/talk_fv_mw3.csv",
+  //"csv/Blitzstein_Stat_110.csv",
+  //"csv/shia.csv",
+  //"csv/debate_nationals_2019.csv"
+]
+
+names = [
+  'TEDX Al gore',
+  'Obama1',
+  'Obama2',
+  'Obama3',
+  'Obama4',
+  'PS: text-n-drive',
+  'PS: mental health days',
+  'PS: school start later',
+  'DS: cube',
+  'DS: hot choc',
+  'DS: cpr',
+  'IS: brain',
+  'IS: star wars',
+  'IS: als',
+  'ES: ali wong',
+  'ES: george carlin',
+  'ES: seinfeld',
+  'Trump speech: 2004',
+  'Trump speech: 2021',
+  'trump_2020_interview',
+  'trump_2010_interview',
+  'obama_2010_interview',
+  'obama_2020_interview',
+  'obama_2004_speech',
+  'obama_2021_speech',
+  'Pres debate: JFK vs Nixon',
+  'Speech sample: JFK',
+  'Speech sample: Nixon',
+  'News sample: Fox 2021',
+  'News sample: NBC 2021',
+  "News sample: CNN debate 2021",
+  "News sample: NBC 2000s",
+  //"News sample: NBC 1990s",
+  "News sample: NBC 1980s",
+  "News sample: NBC 1970s",
+  "News sample: NBC 1960s",
+  "Finale TED Talk 2017",
+  "Finale Zoom Talk 2021",
+  "Martin HBS Interview",
+  "Fernanda HBS Interview",
+  "Viz Talk: 2010",
+  "Viz Talk: 2020",
+  //"Blitzstein Stat 110",
+  //"ShiaLaBeouf the Musical",
+  //"Debate Nationals 2019"
+]
+paths2 = [
+  'csv/algore_ted_ap2.csv',
+  'csv/Weekly Address Time for Congress to Raise the Minimum Wage for the American People_ap2.csv',
+  'csv/Weekly Address The President\'s Budget Ensures Opportunity for All Hard-Working Americans_ap2.csv',
+  'csv/Weekly Address The President\'s Year of Action_ap2.csv',
+  'csv/Weekly Address Wishing the American People a Happy Thanksgiving_ap2.csv',
+  'csv/ps_Texting and Driving Persuasive Speech_ap2.csv',
+  'csv/ps_Why students should have mental health days  Hailey Hardcastle_ap2.csv',
+  'csv/ps_Why school should start later for teens  Wendy Troxel_ap2.csv',
+  'csv/ds_My demonstration speech on how to solve the Rubik\'s cube_ap2.csv',
+  'csv/ds_Demonstrative Speech How to Make Frozen Hot Choocolate_ap2.csv',
+  'csv/ds_CPR Demonstration Speech_ap2.csv',
+  'csv/is_After watching this, your brain will not be the same  Lara Boyd  TEDxVancouver_ap2.csv',
+  'csv/is_Informative Speech Star Wars_ap2.csv',
+  'csv/is_"About ALS" - An Informative Speech_ap2.csv',
+  'csv/es_Ali Wong Stand-Up Performance - Late Night with Seth Meyers_ap2.csv',
+  'csv/es_George Carlin - Everyday expressions (that don\'t make sense)_ap2.csv',
+  'csv/es_Jerry Seinfeld Does His Best Tight Five_ap2.csv',
+  'csv/trump_2004_speech_ap2.csv',
+  'csv/trump_2020_speech_ap2.csv',
+  'csv/trump_2020_interview_ap2.csv',
+  'csv/trump_2010_interview_ap2.csv',
+  'csv/obama_2010_interview_ap2.csv',
+  'csv/obama_2020_interview_ap2.csv',
+  'csv/obama_2004_speech_ap2.csv',
+  'csv/obama_2021_speech_ap2.csv',
+  "csv/kennedy_nixon_debate_part_ap2.csv",
+  "csv/kennedy_rice_ap2.csv",
+  "csv/nixon_vietnam_1969_ap2.csv",
+  "csv/news_fox_ap2.csv",
+  "csv/news_lester_holt_ap2.csv",
+  "csv/news_cnn_debate_ap2.csv",
+  "csv/news_nbc_2000_ap2.csv",
+  //"csv/news_nbc_1990.csv",
+  "csv/news_nbc_1980_ap2.csv",
+  "csv/news_nbc_1970_ap2.csv",
+  "csv/news_nbc_1960_ap2.csv",
+  "csv/finale_talk_2017_ap2.csv",
+  "csv/finale_talk_2021_ap2.csv",
+  "csv/martin_hbs_ap2.csv",
+  "csv/fernanda_hbs_ap2.csv",
+  "csv/viz_talk_2010_ap2.csv",
+  "csv/talk_fv_mw_ap2.csv",
+  //"csv/Blitzstein_Stat_110_ap.csv",
+  //"csv/shia_ap.csv",
+  // "csv/debate_nationals_2019_ap.csv"
+]
+links = [
+  "rDiGYuQicpA",
+  "JYv78x66_E4",
+  "dBek8h5tGMU",
+  "MwRTZcrHwHQ",
+  "nGiUxYBdsFA",
+  "Vc3ovSFpyEk",
+  "1qq7lDL-bzY",
+  "TS6lFDVR-3g",
+  "87Tdm1IasYA",
+  "UF_P514soas",
+  "g6679Fo4qQ4",
+  "LNHBMFCzznE",
+  "GSIppEr0Nws",
+  "QU9ypd9Xxjo",
+  "BEhcDXTr45Q",
+  "wUG0IMjfxCE",
+  "984VkHzXl8w",
+  "gnp6SOqVLLA", //trump 2004 sp
+  "F74MfjZWjI4", //trump 2020 sp
+  "AgEokekPzMU", // trump 2020 int
+  "GB_UrICejIs", // trump 2010 int
+  "A7ZwtxsGbQ0", // obama 2010 int
+  "mAFv55o47ok", // obama 2020 int
+  "eWynt87PaJ0", //obama 2004 sp
+  "9xUgoxagJJA", //obama 2021 sp
+  "AYP8-oxq8ig#t=22m0s", // kennedy nixon
+  "WZyRbnpGyzQ",//kennedy
+  "RPpOBu2LNCo",//nixon
+  "qkGxyfpex8A", // fox news
+  "eCQRhd-_4bw",// nbc lester holt
+  "r1uMiKo9ebc",//cnn debate
+  "aeJ9KPZLvJE", //2000s news
+  //"https://www.youtube.com/watch?v=KLlHcLTFwnM#t=8m07s",//1990 news
+  "g_uEiwCElq0#t=4m06s",//1980 news
+  "ksFhXFuRblg#t=1m26s",//1970 news
+  "OEBg2Ma0hU4",//1960 news
+  "h4lIr8rgo5zE#t=0m12s", //finale ted talk 2017
+  "LU9ejIgpWL0", //finale zoom talk
+  "O_JAZNbj8Pg",//martin hbs
+  "u5JV88yPoGc", //fernanda hbs
+  "zeVODwQAYT8#t=0m48s",//viz talk 2010
+  "jvhxHYKzfa8",//rstudio talk
+  "fDcjhAKuhqQ&t=170s", // blitz stat 110
+  "o0u4M6vppCI",//shia,
+  "Dw6Nvv8UMbU#t=0m37s"//debate 2019
+]

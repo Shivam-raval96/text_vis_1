a6325a09ba50ae2cb1578fca9ce18097243af549
Shivam-raval96 <72898964+Shivam-raval96@users.noreply.github.com>
2022-05-11 09:55:20 -0400

    Add files via upload

---
+++ names1.js
@@ -0,0 +1,52 @@
+paths = [
+  'csv/Bill Gates_ The next outbreak_ We’re not ready _ TED-(480p)3.csv',
+  'csv/algore_ted3.csv',
+  'csv/Celeste Headlee_ 10 ways to have a better conversation _ TED-(480p)3.csv',
+  'csv/Tim Urban_ Inside the mind of a master procrastinator _ TED-(480p)3.csv',
+  'csv/Hyeonseo Lee_ My escape from North Korea _ TED-(480p)3.csv',
+  'csv/How to spot a liar _ Pamela Meyer-(480p)3.csv',
+  'csv/How language shapes the way we think _ Lera Boroditsky-(480p)3.csv',
+  'csv/Do schools kill creativity_ _ Sir Ken Robinson(360p)3.csv',
+  "csv/finale_talk_20173.csv",
+  'csv/Grit_ the power of passion and perseverance _ Angela Lee Duckworth-(480p)3.csv',
+]
+
+names = [
+ 'TED1',
+  'TED2',
+  'TED3',
+  'TED4',
+  'TED5',
+  'TED6',
+  'TED7',
+  'TED8',
+  'TED9',
+  'TED10',
+
+]
+paths2 = [
+  'csv/Bill Gates_ The next outbreak_ We’re not ready _ TED-(480p)_ap2.csv',
+  'csv/algore_ted_ap2.csv',
+  'csv/Celeste Headlee_ 10 ways to have a better conversation _ TED-(480p)_ap2.csv',
+  'csv/Tim Urban_ Inside the mind of a master procrastinator _ TED-(480p)_ap2.csv',
+  'csv/Hyeonseo Lee_ My escape from North Korea _ TED-(480p)_ap2.csv',
+  'csv/How to spot a liar _ Pamela Meyer-(480p)_ap2.csv',
+  'csv/How language shapes the way we think _ Lera Boroditsky-(480p)_ap2.csv',
+  'csv/Do schools kill creativity_ _ Sir Ken Robinson(360p)_ap2.csv',
+  "csv/finale_talk_2017_ap2.csv",
+  'csv/Grit_ the power of passion and perseverance _ Angela Lee Duckworth-(480p)_ap2.csv',
+]
+links = [
+  '6Af6b_wyiwI',
+  "rDiGYuQicpA",
+  'R1vskiVDwl4',
+  'arj7oStGLkU',
+  'PdxPCeWw75k',
+  'P_6vDLq64gE',
+  'RKK7wGAYP6k',
+  'iG9CE55wbtY',
+  '4lIr8rgo5zE',
+  'H14bBuluwB8',
+]
+
+

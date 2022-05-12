40df31ab5f5f275983281885d58ff567909bb727
Shivam-raval96 <sraval@g.harvard.edu>
2022-05-12 08:23:00 -0400

    added tiled ozyviz

---
+++ names_ozy.js
@@ -0,0 +1,113 @@
+paths = [
+  'csv/ozymandias/The Ballad of Buster Scruggs - Ozymandias-(480p)3.csv',
+  'csv/ozymandias/Ozymandias3.csv',
+  'csv/ozymandias/Ozymandias by Percy Bysshe Shelley - Poetry Reading3.csv',
+  'csv/ozymandias/Ozymandias - Percy Bysshe Shelley (GCSE Poetry)-(480p)3.csv',
+  'csv/ozymandias/Ozymandias - by Percy Bysshe Shelley (Read by Jack Donovan)3.csv',
+  'csv/ozymandias/ozy53.csv',
+  'csv/ozymandias/ozy43.csv',
+  'csv/ozymandias/ozy33.csv',
+  'csv/ozymandias/ozy23.csv',
+  'csv/ozymandias/Ozy13.csv',
+  'csv/ozymandias/ozy_vince_price3.csv',
+  'csv/ozymandias/Ozy_tom3.csv',
+  'csv/ozymandias/Ozy_r_attenb3.csv',
+  'csv/ozymandias/Ozy_john_g3.csv',
+  'csv/ozymandias/ozy_elanor_b3.csv',
+  'csv/ozymandias/ozy_darin_h3.csv',
+  'csv/ozymandias/Ozy_bryan3.csv',
+  'csv/ozymandias/ozy_ben_k3.csv',
+  'csv/ozymandias/Ozy_adam3.csv',
+  'csv/ozymandias/Elisabeth Sohier-Poirier_ _Ozymandias_3.csv',
+]
+
+names = [
+  'OZY1',
+  'OZY2',
+  'OZY3',
+  'OZY4',
+  'OZY5',
+  'OZY6',
+  'OZY7',
+  'OZY8',
+  'OZY9',
+  'OZY10',
+  'OZY11',
+  'OZY12',
+  'OZY13',
+  'OZY14',
+  'OZY15',
+  'OZY16',
+  'OZY17',
+  'OZY18',
+  'OZY19',
+  'OZY20',
+]
+
+paths2 = [
+  'csv/ozymandias/The Ballad of Buster Scruggs - Ozymandias-(480p)_ap2.csv',
+  'csv/ozymandias/Ozymandias_ap2.csv',
+  'csv/ozymandias/Ozymandias by Percy Bysshe Shelley - Poetry Reading_ap2.csv',
+  'csv/ozymandias/Ozymandias - Percy Bysshe Shelley (GCSE Poetry)-(480p)_ap2.csv',
+  'csv/ozymandias/Ozymandias - by Percy Bysshe Shelley (Read by Jack Donovan)_ap2.csv',
+  'csv/ozymandias/ozy5_ap2.csv',
+  'csv/ozymandias/ozy4_ap2.csv',
+  'csv/ozymandias/ozy3_ap2.csv',
+  'csv/ozymandias/ozy2_ap2.csv',
+  'csv/ozymandias/Ozy1_ap2.csv',
+  'csv/ozymandias/ozy_vince_price_ap2.csv',
+  'csv/ozymandias/Ozy_tom_ap2.csv',
+  'csv/ozymandias/Ozy_r_attenb_ap2.csv',
+  'csv/ozymandias/Ozy_john_g_ap2.csv',
+  'csv/ozymandias/ozy_elanor_b_ap2.csv',
+  'csv/ozymandias/ozy_darin_h_ap2.csv',
+  'csv/ozymandias/Ozy_bryan_ap2.csv',
+  'csv/ozymandias/ozy_ben_k_ap2.csv',
+  'csv/ozymandias/Ozy_adam_ap2.csv',
+  'csv/ozymandias/Elisabeth Sohier-Poirier_ _Ozymandias__ap2.csv'
+]
+links = [
+
+  "GdQKUvv3bew",
+  "",
+  "BAYk2HX1AWk",
+  "",
+  "sO2vpba_JdA",
+
+  "ezWk5wTr6sU",//mythbuster 5
+  "Tz8VG1zIEL8",// lance foster 4
+  "",//3
+  "V1bbJT7bZVA", //ozy2
+  "bFgw_vhwRdA",// rob simpson 1
+
+  "Tb2RVgLQV-A",
+  "LVTiES1pE2o", // tom o bedlam
+  "bv2nklTyq9Q", //richard a
+  "gHsBcUD_7jw", //sir john gielgud
+  "7TgYh9prPqk", //elanor
+
+  "HfBcLOrm_Gg",//darin hagre
+  "T3dpghfRBHE", //bryanC
+  "krbX-9ugbI4", //benk
+  "",
+  "S7EL0-GZL5U",
+
+
+
+
+
+
+
+
+
+
+
+]
+
+
+
+
+
+
+
+
